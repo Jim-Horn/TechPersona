1100e02e4badc164ece85318030610a9bfd80407
Horn, Jim <Jim.Horn@asurion.com>
2020-08-28 07:18:56 -0700

    Consume and display the elements sent down from the wrapper

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const Results = ({ setSlideshowState }) => {
+const Results = ({ setSlideshowState, els }) => {
     return (
         <span>
             <h1>Complete!</h1>
             <h2>More complete</h2>
+            <pre>{JSON.stringify(els, null, 4)}</pre>
             <button
                 onClick={() => {
                     setSlideshowState(false);
