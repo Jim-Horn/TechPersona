199f66b9709a3443ea6d4bc89a99da4044f128ab
Horn, Jim <Jim.Horn@asurion.com>
2020-09-08 10:42:30 -0700

    Move weights to a separate file

---
+++ src/components/calcs/weights.js
@@ -0,0 +1,41 @@
+export default [
+    {
+        weights: { enthusiast: 0, skeptic: 0, passenger: 0, nearly: 0, diyer: 0 },
+    },
+    {
+        weights: { enthusiast: 1.73, skeptic: -1.799, passenger: -1.72, nearly: 0.341, diyer: 0.692 },
+    },
+    {
+        weights: { enthusiast: -0.039, skeptic: -1.116, passenger: -0.75, nearly: 0.242, diyer: -0.913 },
+    },
+    {
+        weights: { enthusiast: -0.8, skeptic: -1.563, passenger: -2.098, nearly: 0.326, diyer: -1.392 },
+    },
+    {
+        weights: { enthusiast: 0.352, skeptic: 0.139, passenger: -0.355, nearly: 0.167, diyer: 0.251 },
+    },
+    {
+        weights: { enthusiast: 2.802, skeptic: -1.129, passenger: 0.351, nearly: 0.294, diyer: 1.631 },
+    },
+    {
+        weights: { enthusiast: -1.381, skeptic: -3.641, passenger: -3.565, nearly: 0.556, diyer: -2.495 },
+    },
+    {
+        weights: { enthusiast: 0.616, skeptic: -0.2038, passenger: -3.05, nearly: 0.358, diyer: 0.185 },
+    },
+    {
+        weights: { enthusiast: -0.517, skeptic: -1.926, passenger: -1.519, nearly: 0.329, diyer: -1.459 },
+    },
+    {
+        weights: { enthusiast: 0.347, skeptic: -0.252, passenger: -0.838, nearly: 0.375, diyer: 0.256 },
+    },
+    {
+        weights: { enthusiast: -0.12, skeptic: -0.295, passenger: -0.93, nearly: -0.06, diyer: -0.007 },
+    },
+    {
+        weights: { enthusiast: 0.428, skeptic: -3.293, passenger: -2.277, nearly: 0.168, diyer: -2.224 },
+    },
+    {
+        weights: { enthusiast: 2.417, skeptic: -1.049, passenger: 0.179, nearly: 0.333, diyer: 1.472 },
+    },
+];
