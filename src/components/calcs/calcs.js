82fc5712d9768a1f2050ef2b509e30d592b06513
Horn, Jim <Jim.Horn@asurion.com>
2020-09-30 12:58:57 -0700

    Calculations "sorta" work

--- src/components/calcs/calcs.js
+++ src/components/calcs/calcs.js
@@ -1,5 +1,8 @@
 import { mathFns } from '../../utils';
+import { personas } from '../../constants';
+
 export default function (arr) {
+    console.log('arr: ', arr);
     if (arr.length !== 13) {
         return { error: `expected an Array with 13 elements, received ${arr.length}` };
     }
@@ -11,8 +14,34 @@ export default function (arr) {
         return { error: `expected an Array with 13 digits, received ${ints.length}: ${ints}` };
     }
 
+    function calcValues(arr) {
+        const personaValues = arr.map(addValues);
+        const results = {};
+
+        Object.keys(personas).forEach(
+            (persona) => (results[persona] = personaValues.reduce((acc, el) => el[persona], 0))
+        );
+        return { personaValues, results };
+
+        function addValues(item) {
+            Object.keys(item.weights).forEach((el) => (item[el] = item.value * item.weights[el]));
+
+            return item;
+        }
+    }
+
+    function getMax(obj) {
+        return Object.keys(obj).reduce((a, b) => (obj[a] > obj[b] ? a : b));
+    }
+
+    const calcs = calcValues(arr);
+
     return {
-        mean: mathFns.avg(arr),
-        stdDev: mathFns.stdDev(arr),
+        mean: mathFns.avg(ints),
+        stdDev: mathFns.stdDev(ints),
+        initialValues: ints,
+        personaValues: calcs.personaValues,
+        results: calcs.results,
+        persona: getMax(calcs.results),
     };
 }
