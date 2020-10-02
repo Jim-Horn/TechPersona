8683adf9c8033f1646c15249914e60947386726b
Horn, Jim <Jim.Horn@asurion.com>
2020-10-02 05:56:16 -0700

    Testing uppercase c (wondering if there was some type of conflict)

--- src/constants.js
+++ src/constants.js
@@ -11,9 +11,9 @@ export const CONSTANTS = {
 };
 
 export const personas = {
-    enthusiast: { friendlyName: 'Enthusiast', determiner: 'an', constant: -10.39294049 },
-    skeptic: { friendlyName: 'Skeptic', determiner: 'a', constant: -8.395134809 },
-    passenger: { friendlyName: 'Passenger', determiner: 'a', constant: -12.6347289 },
-    nearly: { friendlyName: 'Nearly', determiner: 'a', constant: -0.654191647 },
-    diyer: { friendlyName: 'DIYer', determiner: 'a', constant: -10.25148354 },
+    enthusiast: { friendlyName: 'Enthusiast', determiner: 'an', Constant: -10.39294049 },
+    skeptic: { friendlyName: 'Skeptic', determiner: 'a', Constant: -8.395134809 },
+    passenger: { friendlyName: 'Passenger', determiner: 'a', Constant: -12.6347289 },
+    nearly: { friendlyName: 'Nearly', determiner: 'a', Constant: -0.654191647 },
+    diyer: { friendlyName: 'DIYer', determiner: 'a', Constant: -10.25148354 },
 };
