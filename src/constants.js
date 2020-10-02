09d535648ac7b28ee7777a419208495cc02d2585
Horn, Jim <Jim.Horn@asurion.com>
2020-10-02 10:02:50 -0700

    Correct the freaking constants! GODDDAMMMMIT!!!!

--- src/constants.js
+++ src/constants.js
@@ -11,9 +11,9 @@ export const CONSTANTS = {
 };
 
 export const personas = {
-    enthusiast: { friendlyName: 'Enthusiast', determiner: 'an', Constant: -10.39294049 },
-    skeptic: { friendlyName: 'Skeptic', determiner: 'a', Constant: -8.395134809 },
-    passenger: { friendlyName: 'Passenger', determiner: 'a', Constant: -12.6347289 },
-    nearly: { friendlyName: 'Nearly', determiner: 'a', Constant: -0.654191647 },
-    diyer: { friendlyName: 'DIYer', determiner: 'a', Constant: -10.25148354 },
+    enthusiast: { friendlyName: 'Enthusiast', determiner: 'an', Constant: -6.558 },
+    skeptic: { friendlyName: 'Skeptic', determiner: 'a', Constant: -6.178 },
+    passenger: { friendlyName: 'Passenger', determiner: 'a', Constant: -6.193 },
+    nearly: { friendlyName: 'Nearly Adopter', determiner: 'a', Constant: -1.703 },
+    diyer: { friendlyName: 'Do-it-Yourselfer', determiner: 'a', Constant: -6.985 },
 };
